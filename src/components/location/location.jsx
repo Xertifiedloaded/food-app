0f9edec173f8ae961688f7f84520eddb3a4cce38
xertifiedloaded <certifiedloaded@yahoo.com>
2023-04-08 17:31:33 +0100

    my changes

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -3,11 +3,18 @@ import classes from "./location.module.css";
 import { userContext } from "../../mainLayout/mainLayout";
 import { LocationData } from "../../constant";
 
-const Location = ({ defaultValue, setDefaultValue }) => {
+const Location = ({
+  defaultValue,
+  setDefaultValue,
+  initialLocation,
+  setInitialLocation,
+}) => {
   const [clicked, setClicked] = useState(false);
 
-  const handleSelect = (data) => {
-    setDefaultValue(data.price);
+  const handleSelect = (item) => {
+    setDefaultValue(item.price);
+    setInitialLocation(item.location);
+    
   };
 
   const { toggle, active } = useContext(userContext);
@@ -15,13 +22,29 @@ const Location = ({ defaultValue, setDefaultValue }) => {
     <>
       <div className={classes.all}>
         <div>
-          <div className={classes.legend} onClick={toggle}>
-            <span>location</span>
-            <span>${defaultValue == "" ? 0 : defaultValue}</span>
+          <div className={classes.legend}>
+            <div className={classes.inputs}>
+              <legend>location</legend>
+              <input
+                onClick={toggle}
+                type="text"
+                value={
+                  initialLocation === ""
+                    ? "Select Your Location"
+                    : initialLocation
+                }
+              />
+            </div>
           </div>
           {active && (
             <div className={classes.main}>
-              <ClientLocation handleSelect={handleSelect} />
+              {LocationData.map((item, i) => (
+                <ClientLocation
+                  item={item}
+                  key={i}
+                  handleSelect={handleSelect}
+                />
+              ))}
             </div>
           )}
         </div>
@@ -31,19 +54,20 @@ const Location = ({ defaultValue, setDefaultValue }) => {
 };
 export default Location;
 
-const ClientLocation = ({ handleSelect }) => {
+const ClientLocation = ({ handleSelect, item, i, key }) => {
   return (
     <>
-      {LocationData.map((item, i) => (
-        <div
-          className={classes.Items}
-          key={i}
-          onClick={() => handleSelect(item)}
-        >
-          <span>{item.location}</span>
-          <span>${item.price}</span>
-        </div>
-      ))}
+      <div
+        className={classes.Items}
+        key={item.id}
+        onClick={() =>
+          handleSelect(item)
+    
+        }
+      >
+        <span>{item.location}</span>
+        <span>${item.price}</span>
+      </div>
     </>
   );
 };
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,12 +2,39 @@ import { useContext, useState } from "react";
 import classes from "./modal.module.css";
 import { userContext } from "../../mainLayout/mainLayout";
 import Location from "../location/location";
-const Modal = ({}) => {
+const Modal = ({ items, key }) => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleName = (e) => {
+    setName(() => ({
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleAddress = (e) => {
+    setAddress(() => ({
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    console.log(address);
+    const WhatsAppUrl =
+      "https://wa.me/2348168043011?text=" +
+      `${defaultValue} + ${JSON.stringify(name)}+ "%0a" + ${JSON.stringify(address)} + ${JSON.stringify(cartItems)}` +
+      "%0a";
+    // console.log(cartItems);
+
+    return window.open(WhatsAppUrl);
+  };
+
   const [defaultValue, setDefaultValue] = useState("");
+  const [initialLocation, setInitialLocation] = useState("");
   const { cartItems, AddToCart, removeFromCart, toggleModal } =
     useContext(userContext);
   const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
-  const BankCharges = itemsPrice * 0.14;
+  const BankCharges = 20;
   const Delivery = defaultValue;
   const totalPrice = itemsPrice + BankCharges + Delivery;
   return (
@@ -38,22 +65,48 @@ const Modal = ({}) => {
                 <p>item price</p>${itemsPrice.toFixed(2)}
               </div>
               <div className={classes.totalPrice}>
-                <p>Bank price</p>${BankCharges.toFixed(2)}
+                <p>Bank Charges</p>${BankCharges.toFixed(2)}
               </div>
               <div className={classes.totalPrice}>
-                <p>Delivery</p>${Delivery}
+                <p>Delivery Fee</p>
+                {defaultValue === "" ? 0 : `$${defaultValue}`}
               </div>
               <Location
                 defaultValue={defaultValue}
                 setDefaultValue={setDefaultValue}
+                initialLocation={initialLocation}
+                setInitialLocation={setInitialLocation}
               />
+              <form action="">
+                <div className={classes.inputs}>
+                  <legend>FullName</legend>
+                  <input
+                    name="name"
+                    type="text"
+                    value={name.name}
+                    onChange={handleName}
+                    placeholder="enter your Name"
+                  />
+                </div>
+                <div className={classes.inputs}>
+                  <legend>Address</legend>
+                  <input
+                    type="text"
+                    name="location"
+                    value={name.location}
+                    onChange={handleAddress}
+                    placeholder="enter proper description of your location"
+                  />
+                </div>
+              </form>
               <div className={classes.totalPrice}>
                 <p className={classes.total}>Total Amount</p>
                 <p className={classes.totalAmt}> ${Math.floor(totalPrice)}</p>
               </div>
+
               <div className={classes.btnContainer}>
                 <button onClick={toggleModal}>close</button>
-                <button>order</button>
+                <button onClick={HandleSubmit}>order</button>
               </div>
             </div>
           )}
@@ -64,14 +117,14 @@ const Modal = ({}) => {
 };
 
 export default Modal;
-const ModalCart = ({ AddToCart, removeFromCart, items, key }) => {
+const ModalCart = ({ AddToCart, removeFromCart, items, key, price }) => {
   return (
     <>
       <div key={key} className={classes.items}>
         <div className={classes.content}>
           <p className={classes.name}>{items.name}</p>
           <div className={classes.price}>
-            <p>${items.price}</p>
+            <p price={price}>${items.price}</p>
             <p>x{items.qty}</p>
           </div>
         </div>
@@ -80,7 +133,6 @@ const ModalCart = ({ AddToCart, removeFromCart, items, key }) => {
           <button onClick={() => removeFromCart(items)}>-</button>
         </div>
       </div>
-      <div>{/* ${items.price.toFixed(2)} */}</div>
     </>
   );
 };
